d2f46fdd039afa4f4dd81b9b3c9de810884b4fb7
dsequera <dasequerap@gmail.com>
2019-11-14 15:42:37 -0500

    added new tests for single people query with mapping configuration

--- src/test/java/tests/PeopleSingleTests.java
+++ src/test/java/tests/PeopleSingleTests.java
@@ -1,11 +1,13 @@
 package tests;
 
 import helpers.config.MappingReader;
+import models.PeopleModel;
+import views.PeopleView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMethod;
-import views.PeopleView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,29 +15,53 @@ import java.util.ArrayList;
 class PeopleSingleTests extends BaseTests{
 
     private final PeopleView peopleView = new PeopleView();
+    private final int peopleId = 1;
+    private final MappingReader mappingReader = new MappingReader();
+    private final MappingReader.Resources peopleResource = MappingReader
+            .Resources.PEOPLE;
+    private ArrayList<String> peopleSingleResponseFields = new ArrayList<>();
+    private PeopleModel people = null;
 
     PeopleSingleTests() throws IOException {
-        MappingReader mapping = new MappingReader();
-        MappingReader.Resources peopleResource = MappingReader
-                .Resources.PEOPLE;
-        this.setCurrentResponse(peopleView.getPeople(1));
-        ArrayList<String> peopleSingleResponseFields = mapping
-                .getMandatoryFieldNames( peopleResource, RequestMethod.GET, true );
+        this.setCurrentResponse(peopleView.getPeopleById(peopleId));
+        this.people = (PeopleModel) this.getCurrentResponse().extract()
+                .jsonPath().getObject("$", PeopleModel.class);
+        this.peopleSingleResponseFields = mappingReader
+                .getMandatoryFieldNames(this.peopleResource, RequestMethod.GET, true );
     }
 
-    @Order(10)
+    @Order(1)
     @Test
     @DisplayName("Check if response is successful for specifics people")
     void checkSpecificPeopleResponseOkTest() {
-        this.setCurrentResponse(peopleView.getPeopleById(1));
-        this.validateResponseStatusByCode(200);
+        this.setCurrentResponse(peopleView.getPeopleById(peopleId));
+        this.validateResponseStatusByCode(
+                mappingReader.getExpectedResponseCode(this.peopleResource, HttpMethod.GET));
     }
 
-    @Order(11)
+    @Order(2)
     @Test
     @DisplayName("Check if response format is JSON for specific people")
     void checkSpecificPeopleResponseFormat(){
         this.setCurrentResponse(peopleView.getPeopleById(1));
         this.validateContentTypeIsJson();
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Check if single people query has mandatory fields")
+    void checkIfPeopleResponseHasMandatoryFields(){
+        for(String field: peopleSingleResponseFields){
+            this.validateFieldIsPresent(field);
+        }
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Check if mandatory fields are not null or empty")
+    void checkIfPeopleMandatoryFieldsAreNotNullOrEmpty(){
+        for(String field: peopleSingleResponseFields){
+            this.validateFieldHasNotValue(field, null);
+        }
     }
 }
