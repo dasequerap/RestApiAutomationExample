5bd41c7562575bbb1df6b86dab28b77bf846e5ea
dsequera <dasequerap@gmail.com>
2019-12-02 10:28:11 -0500

    Changed loader model to use jackson library instead jsefa

--- src/test/java/models/ModelLoader.java
+++ src/test/java/models/ModelLoader.java
@@ -1,27 +1,40 @@
 package models;
 
+import java.util.ArrayList;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import helpers.ResourceReader;
-import net.sf.jsefa.Deserializer;
-import net.sf.jsefa.csv.CsvIOFactory;
 import java.io.*;
 
 public class ModelLoader{
-    private String _fileContents;
+    private ResourceReader _peopleData;
+    private InputStream _file;
 
     public ModelLoader() throws IOException {
         String _csvFile = "People.csv";
-        ResourceReader _peopleData = new ResourceReader( _csvFile, ResourceReader.ProjectResource.TEST_DATA );
-        _fileContents = _peopleData.getFileContents();
+        ResourceReader _peopleData = new ResourceReader( _csvFile, ResourceReader.ProjectResource.TEST_DATA);
+        String _fileContents = _peopleData.getFileContents();
+        _file = _peopleData.getFile( _csvFile, ResourceReader.ProjectResource.TEST_DATA);
         System.out.println( _fileContents );
     }
 
-    public void getTestPeople(){
-        Deserializer deserializer = CsvIOFactory.createFactory(PeopleModel.class).createDeserializer();
-        StringReader reader = new StringReader( _fileContents );
-        deserializer.open(reader);
-        while (deserializer.hasNext()) {
-            PeopleModel people = deserializer.next();
+    public void getTestPeople() throws IOException {
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = mapper.schemaFor(PeopleModel.class);
+        schema = schema.withColumnSeparator(';').withUseHeader(true)
+                .withHeader().withArrayElementSeparator(",");
+        ObjectReader reader = mapper.readerFor(PeopleModel.class).with(schema);
+        MappingIterator<PeopleModel> iterator = reader.readValues(_file);
+        ArrayList<PeopleModel> testPeople = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            testPeople.add(iterator.next());
         }
-        deserializer.close(true);
+
+        for(PeopleModel people: testPeople){
+            System.out.println(people.getName());
+        }
     }
 }
